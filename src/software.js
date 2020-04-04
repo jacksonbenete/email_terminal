ef76c2da0e26109c137cd7d62635da2aa7838de3
Jackson B Ferreira <jacksonbenete@gmail.com>
2020-04-03 23:56:52 -0300

    Help improved
    The help command function has been improved to show help for another commands.

--- src/software.js
+++ src/software.js
@@ -122,9 +122,84 @@ var system = {
         })
     },
 
-    help: function(){
+    help: function(args){
         return new Promise(function(resolve, reject) {
-            resolve('<div class="ls-files">' + CMDS_.join('<br>') + '</div>')
+            if (args == ''){
+                resolve([
+                    `You can read the help of a specific command by entering as follows: 'help commandName'`,
+                    `List of useful commands:`,
+                    '<div class="ls-files">' + CMDS_.join('<br>') + '</div>'
+                ])
+            }
+            if (args[0] == 'clear'){
+                resolve([
+                    `Usage:`,
+                    `> clear`,
+                    `The clear command will completely wipeout the entire screen, but it will not affect the history or whatever have been made into the terminal.`
+                ])
+            }
+            if (args[0] == 'echo'){
+                resolve([
+                    `Usage:`,
+                    `> echo args`,
+                    `The echo command will print args into terminal.`
+                ])
+            }
+            if (args[0] == 'login'){
+                resolve([
+                    `Usage:`,
+                    `> login username@password`,
+                    `If you're a registered user into the server, you can login to access your data files and messages.`
+                ])
+            }
+            if (args[0] == 'read'){
+                resolve([
+                    `Usage:`,
+                    `> read x`,
+                    `If you're logged in you can read your mail messages if any.`
+                ])
+            }
+            if (args[0] == 'telnet'){
+                resolve([
+                    `Usage:`,
+                    `> telnet address`,
+                    `> telnet address@password`,
+                    `You can connect to a valid address to access a specific server if the server is at internet.`,
+                    `Intranet servers can only be accessed locally.`,
+                    `You may need a password if it isn't a public server.`
+                ])
+            }
+            if (args[0] == 'date'){
+                resolve([
+                    `Usage:`,
+                    `> date`,
+                    `The date command will print the current date-time into terminal.`
+                ])
+            }
+            if (args[0] == 'help'){
+                resolve([
+                    `Usage:`,
+                    `> help`,
+                    `The default help message.`,
+                    `It will show some of the available commands in a server.`,
+                    `Note that some existent commands may not have been show at help message.`
+                ])
+            }
+            if (args[0] == 'mail'){
+                resolve([
+                    `Usage:`,
+                    `> mail`,
+                    `If you're logged in you can list your mail messages if any.`
+                ])
+            }
+            if (args[0] == 'ping'){
+                resolve([
+                    `Usage:`,
+                    `> ping address`,
+                    `The ping command will try to reach a valid address.`,
+                    `If the ping doesn't return a valid response, the address may be incorrect, may not exist or can't be reached locally.`
+                ])
+            }
         })
     },
     
