724d7ab95fbce81e71d54ec1dda1e4e5b1d325df
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-02-09 11:58:44 +0100

    Moving from JSON dicts to HTML strings in software "message" field + introducing optional "secretCommand" field

--- src/glitch-img.js
+++ src/glitch-img.js
@@ -13,7 +13,7 @@ function glitchImage( imgElem ) { /* eslint-disable-line no-unused-vars */
         new p5( ( sketch ) => {
             // First ensure <img> is loaded, in order to have access to its .width & .height:
             sketch.loadImage( imgElem.src, ( loadedImg ) => {
-                const canvas = sketch.createCanvas( loadedImg.width * 2, loadedImg.height * 2 );
+                const canvas = sketch.createCanvas( loadedImg.width, loadedImg.height );
                 canvas.parent( imgElem.parentNode ); // Positioning canvas
                 // Copy CSS classes from <img>:
                 canvas.elt.classList.add( ...imgElem.classList );
@@ -35,58 +35,56 @@ function glitchImage( imgElem ) { /* eslint-disable-line no-unused-vars */
     } );
 }
 
+const CHANNEL_LEN = 4; // Constant from the doc: https://p5js.org/reference/#/p5.Image
+
 class Glitch {
     constructor( img, p5sketch ) {
         this.p5 = p5sketch;
-        this.channelLen = 4;
         this.imgOrigin = img;
         this.imgOrigin.loadPixels();
-        this.copyData = [];
+        this.copyData = new Uint8ClampedArray( this.imgOrigin.pixels );
         this.flowLineImgs = [];
         this.shiftLineImgs = [];
         this.shiftRGBs = [];
         this.scatImgs = [];
         this.throughFlag = true;
-        this.copyData = new Uint8ClampedArray( this.imgOrigin.pixels );
 
         // flow line
         for ( let i = 0; i < 1; i++ ) {
-            const o = {
+            this.flowLineImgs.push( {
                 pixels: null,
                 t1: this.p5.floor( this.p5.random( 0, 1000 ) ),
                 speed: this.p5.floor( this.p5.random( 4, 24 ) ),
                 randX: this.p5.floor( this.p5.random( 24, 80 ) )
-            };
-            this.flowLineImgs.push( o );
+            } );
         }
 
         // shift line
         for ( let i = 0; i < 6; i++ ) {
-            const o = null;
-            this.shiftLineImgs.push( o );
+            this.shiftLineImgs.push( null );
         }
 
         // shift RGB
         for ( let i = 0; i < 1; i++ ) {
-            const o = null;
-            this.shiftRGBs.push( o );
+            this.shiftRGBs.push( null );
         }
 
         // scat imgs
         for ( let i = 0; i < 3; i++ ) {
-            const scatImg = {
+            this.scatImgs.push( {
                 img: null,
                 x: 0,
                 y: 0
-            };
-            this.scatImgs.push( scatImg );
+            } );
         }
     }
 
-    replaceData( destImg, srcPixels ) {
+    replaceData( destImg, srcPixels ) { /* eslint-disable-line class-methods-use-this */
+        // Note (Lucas - 2022/02/09): This method is the current app bottleneck, based on Firefox profiling tab
+
         for ( let y = 0; y < destImg.height; y++ ) {
             for ( let x = 0; x < destImg.width; x++ ) {
-                const index = ( y * destImg.width + x ) * this.channelLen;
+                const index = ( y * destImg.width + x ) * CHANNEL_LEN;
                 const r = index;
                 const g = index + 1;
                 const b = index + 2;
@@ -97,6 +95,12 @@ class Glitch {
                 destImg.pixels[ a ] = srcPixels[ a ];
             }
         }
+
+        //   Note (Lucas - 2022/02/09): could be faster but does not behave the same as the lines above :(
+        // destImg.pixels = new Uint8ClampedArray( srcPixels );
+        //   Seems to me that the best improvement forward, performance-wise, is to use a p5.Image or p5.Graphics instead of a Uint8ClampedArray in this.copyData,
+        //   in order to benefit from its (potentially more efficent) copyData method.
+
         destImg.updatePixels();
     }
 
@@ -109,7 +113,7 @@ class Glitch {
         for ( let y = 0; y < srcImg.height; y++ ) {
             if ( tempY === y ) {
                 for ( let x = 0; x < srcImg.width; x++ ) {
-                    const index = ( y * srcImg.width + x ) * this.channelLen;
+                    const index = ( y * srcImg.width + x ) * CHANNEL_LEN;
                     const r = index;
                     const g = index + 1;
                     const b = index + 2;
@@ -129,12 +133,12 @@ class Glitch {
         const rangeH = srcImg.height;
         const rangeMin = this.p5.floor( this.p5.random( 0, rangeH ) );
         const rangeMax = rangeMin + this.p5.floor( this.p5.random( 1, rangeH - rangeMin ) );
-        const offsetX = this.channelLen * this.p5.floor( this.p5.random( -40, 40 ) );
+        const offsetX = CHANNEL_LEN * this.p5.floor( this.p5.random( -40, 40 ) );
 
         for ( let y = 0; y < srcImg.height; y++ ) {
             if ( y > rangeMin && y < rangeMax ) {
                 for ( let x = 0; x < srcImg.width; x++ ) {
-                    const index = ( y * srcImg.width + x ) * this.channelLen;
+                    const index = ( y * srcImg.width + x ) * CHANNEL_LEN;
                     const r = index;
                     const g = index + 1;
                     const b = index + 2;
@@ -155,13 +159,13 @@ class Glitch {
     shiftRGB( srcImg ) {
         const range = 16;
         const destPixels = new Uint8ClampedArray( srcImg.pixels );
-        const randR = ( this.p5.floor( this.p5.random( -range, range ) ) * srcImg.width + this.p5.floor( this.p5.random( -range, range ) ) ) * this.channelLen;
-        const randG = ( this.p5.floor( this.p5.random( -range, range ) ) * srcImg.width + this.p5.floor( this.p5.random( -range, range ) ) ) * this.channelLen;
-        const randB = ( this.p5.floor( this.p5.random( -range, range ) ) * srcImg.width + this.p5.floor( this.p5.random( -range, range ) ) ) * this.channelLen;
+        const randR = ( this.p5.floor( this.p5.random( -range, range ) ) * srcImg.width + this.p5.floor( this.p5.random( -range, range ) ) ) * CHANNEL_LEN;
+        const randG = ( this.p5.floor( this.p5.random( -range, range ) ) * srcImg.width + this.p5.floor( this.p5.random( -range, range ) ) ) * CHANNEL_LEN;
+        const randB = ( this.p5.floor( this.p5.random( -range, range ) ) * srcImg.width + this.p5.floor( this.p5.random( -range, range ) ) ) * CHANNEL_LEN;
 
         for ( let y = 0; y < srcImg.height; y++ ) {
             for ( let x = 0; x < srcImg.width; x++ ) {
-                const index = ( y * srcImg.width + x ) * this.channelLen;
+                const index = ( y * srcImg.width + x ) * CHANNEL_LEN;
                 const r = index;
                 const g = index + 1;
                 const b = index + 2;
@@ -190,7 +194,10 @@ class Glitch {
     }
 
     show() {
+        const showStartTime = window.performance.now();
         const sketch = this.p5;
+        const width = this.imgOrigin.width;
+        const height = this.imgOrigin.height;
 
         // restore the original state
         this.replaceData( this.imgOrigin, this.copyData );
@@ -203,9 +210,6 @@ class Glitch {
                 this.throughFlag = true;
             }, this.p5.floor( this.p5.random( 40, 400 ) ) );
         }
-
-        const width = this.imgOrigin.width;
-        const height = this.imgOrigin.height;
 
         if ( !this.throughFlag ) {
             sketch.push();
@@ -260,5 +264,8 @@ class Glitch {
             }
             sketch.pop();
         } );
+
+        const showDuration = window.performance.now() - showStartTime;
+        console.debug( `Glitch.show() duration=${ showDuration }ms FPS: ${ sketch.frameRate().toFixed( 2 ) }` );
     }
 }
