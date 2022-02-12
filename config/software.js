f5e3576fc94b6cc345d2a25dcd0ffde1aa1d39ec
Lucas Cimon <925560+Lucas-C@users.noreply.github.com>
2022-02-12 16:14:15 +0100

    Fixing a bug due to the use of typeof

--- config/software.js
+++ config/software.js
@@ -1,15 +1,13 @@
 /*
  This file contains the logic for custom software programs
- that perform more complex actions than just printing text.
+ that perform more complex actions than just displaying some text or HTML.
 
  You are invited to edit this file to define your own commands!
- Start by removing the demonstration ones that you don't need for your game.
+ Start by removing the demo ones that you don't need for your game.
 
  Remember that function names must match the names of the programs in software.json.
  */
 /* eslint-disable no-inner-declarations, no-nested-ternary, no-sequences, no-unused-vars */
-
-// TODO: redact doc on this + no "location"
 
 function decrypt( args ) { // The same function can be used to encode text
     if ( args.length === 0 ) {
